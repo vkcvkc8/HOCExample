31cdf16a67124fd6d2bdb16e151cb14b12473c7e
vkcvkc8 <43770656+vkcvkc8@users.noreply.github.com>
2023-09-16 08:28:18 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import './style.css';
+
+// Define the HOC
+function withExtraProp(WrappedComponent) {
+  return function EnhancedComponent(props) {
+    return (
+      <WrappedComponent
+        extraProp="Hello from HOC"
+        lextraProp="lextra"
+        {...props}
+      />
+    );
+  };
+}
+
+function MyComponent(props) {
+  return (
+    <div>
+      <p>{props.extraProp}</p>
+      <p>{props.message}</p>
+    </div>
+  );
+}
+
+function MyComponent2(props) {
+  return (
+    <div>
+      <p>{props.lextraProp}</p>
+      <p>{props.message}</p>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {/* Use the HOC directly within JSX */}
+      {withExtraProp(MyComponent)({ message: 'Hi there' })}
+      {withExtraProp(MyComponent2)({ message: 'Bi there' })}
     </div>
   );
 }
